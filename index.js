6b8009f9f9621bb72642212e4faf4c69f0127b69
GiladVaknin <77235037+GiladVaknin@users.noreply.github.com>
2021-01-24 19:54:36 +0200

    Finished

--- index.js
+++ index.js
@@ -86,42 +86,88 @@ for (let object of objects) {
     minute: "2-digit",
   });
 }
-document.write("<table>");
-document.write("<th>Topic</th>");
-document.write("<th>Tasks Finished %</th>");
-document.write("<th>Tasks Finished</th>");
-document.write("<th>Tasks Given</th>");
-document.write("<th>Total Time</th>");
-document.write("<th>Finished At</th>");
-document.write("<th>Started At</th>");
-for (let i = 0; i < objects.length; i++) {
-  let p = objects[i].percentDone;
-  let t = objects[i].totalTime;
-  let classNamePercent;
-  let classNameTime;
-  if (t <= 24) {
-    classNameTime = "fast";
-  } else if (80 >= t) {
-    classNameTime = "med";
-  } else {
-    classNameTime = "slow";
-  }
-  if (p <= 50) {
-    classNamePercent = "bad";
-  } else if (85 > p) {
-    classNamePercent = "good";
-  } else {
-    classNamePercent = "avg";
-  }
 
-  document.write(`<tr>
-  <td class="topic">${objects[i].topic}</td>
-  <td class="${classNamePercent}">${objects[i].percentDone}%</td>
-  <td>${objects[i].tasksFinished}</td>
-  <td>${objects[i].tasksGiven}</td>
-  <td class="${classNameTime}">${objects[i].totalTime}</td>
-  <td>${objects[i].finishedAt}</td>
-  <td>${objects[i].startedAt}</td>
-  </tr>`);
+let body = document.getElementsByTagName("body")[0];
+let tbl = document.createElement("table");
+let hrow = document.createElement("tr");
+let cell1 = document.createElement("th");
+let cell2 = document.createElement("th");
+let cell3 = document.createElement("th");
+let cell4 = document.createElement("th");
+let cell5 = document.createElement("th");
+let cell6 = document.createElement("th");
+let cell7 = document.createElement("th");
+cell1.innerText = "Topic";
+cell2.innerText = "Tasks Finished%";
+cell3.innerText = "Tasks Finished";
+cell4.innerText = "Tasks Given";
+cell5.innerText = "Total Time Spent";
+cell6.innerText = "Finished At";
+cell7.innerText = "Started At";
+hrow.append(cell1);
+hrow.append(cell2);
+hrow.append(cell3);
+hrow.append(cell4);
+hrow.append(cell5);
+hrow.append(cell6);
+hrow.append(cell7);
+tbl.append(hrow);
+
+for (let i = 0; i < objects.length; i++) {
+  let row = document.createElement("tr");
+  for (let j = 0; j < 7; j++) {
+    let cell = document.createElement("td");
+    let cellText = PropNum(objects[i], j);
+    if (j === 1) {
+      cellText += "%";
+      let p = objects[i].percentDone;
+      cell.className = perBGColor(p);
+    }
+    if (j === 4) {
+      let t = objects[i].totalTime;
+      cell.className = timeBGColor(t);
+    }
+
+    cell.innerText = cellText;
+    row.append(cell);
+  }
+  tbl.append(row);
 }
-document.write("</table>");
+body.append(tbl);
+
+function PropNum(object, num) {
+  switch (num < 7) {
+    case num === 0:
+      return object.topic;
+    case num === 1:
+      return object.percentDone;
+    case num === 2:
+      return object.tasksFinished;
+    case num === 3:
+      return object.tasksGiven;
+    case num === 4:
+      return object.totalTime;
+    case num === 5:
+      return object.finishedAt;
+    case num === 6:
+      return object.startedAt;
+  }
+}
+function perBGColor(num) {
+  if (num <= 50) {
+    return "bad";
+  } else if (85 > num) {
+    return "good";
+  } else {
+    return "avg";
+  }
+}
+function timeBGColor(t) {
+  if (t <= 24) {
+    return "fast";
+  } else if (80 >= t) {
+    return "med";
+  } else {
+    return "slow";
+  }
+}
